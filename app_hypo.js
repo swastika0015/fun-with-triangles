50e0d7bcc04fb408a206388113b1b66a8b7ba1a9
Swastika0015 <swastikayadav5358@gmail.com>
2021-09-24 03:42:30 +0530

    hypotenuse added

---
+++ app_hypo.js
@@ -0,0 +1,11 @@
+const A1 = document.querySelector("#angle1");
+const A2 = document.querySelector("#angle2");
+const A3 = document.querySelector("#angle3");
+const btnCheck = document.querySelector("#btn");
+const message = document.querySelector("#show-msg")
+
+btnCheck.addEventListener("click", function isTriangle(){
+    a1 = parseInt(A1.value)
+    a2 = parseInt(A2.value)
+    a3 = parseInt(A3.value)
+});
